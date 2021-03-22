0dc726aa2efc1bf923f646de41737fd074f15519
grish2018 <grigorisargsyan2016@gmail.com>
2021-03-22 20:27:45 +0100

    style: class name fixies

--- src/pages/SignIn/index.vue
+++ src/pages/SignIn/index.vue
@@ -2,23 +2,23 @@
   <div class="sign-in">
     <div class="sign-in__form-wrapper">
       <form class="sign-in__form" @submit.prevent="submit">
-        <span class="sign-in__form__header"> Вход </span>
-        <div class="sign-in__form__input">
+        <span class="sign-in__form-header"> Вход </span>
+        <div class="sign-in__input-wrapper">
           <input
             v-model="email"
             required
-            class="sign-in__form__input__input"
+            class="sign-in__form-input"
             type="email"
             placeholder="Email"
             autocomplete="email"
             id="email"
           />
         </div>
-        <div class="sign-in__form__input">
+        <div class="sign-in__input-wrapper">
           <input
             v-model="password"
             required
-            class="sign-in__form__input__input"
+            class="sign-in__form-input"
             type="password"
             placeholder="Password"
             autocomplete="current-password"
@@ -79,39 +79,34 @@ export default defineComponent({
     display: flex;
     flex-direction: column;
     margin-bottom: 40px;
-    &__header {
+    &-header {
       font-weight: 300;
       font-size: 26px;
       line-height: 31px;
       color: #101d94;
       margin-bottom: 85px;
     }
-    &__input {
-      display: flex;
-      flex-direction: column;
-      margin-bottom: 30px;
 
-      &__input {
-        width: 100%;
-        height: 40px;
-        border: 1px solid #dedcdc;
+    &-input {
+      width: 100%;
+      height: 40px;
+      border: 1px solid #dedcdc;
+      font-weight: 300;
+      font-size: 16px;
+      line-height: 18px;
+      color: black;
+      border-radius: 5px;
+      transition: all 0.3s;
+      outline: none;
+      padding: 0px 15px;
+      &:invalid {
+        border: 1px solid red;
+      }
+      &::placeholder {
         font-weight: 300;
         font-size: 16px;
         line-height: 18px;
-        color: black;
-        border-radius: 5px;
-        transition: all 0.3s;
-        outline: none;
-        padding: 0px 15px;
-        &:invalid {
-          border: 1px solid red;
-        }
-        &::placeholder {
-          font-weight: 300;
-          font-size: 16px;
-          line-height: 18px;
-          color: #6b6b6b;
-        }
+        color: #6b6b6b;
       }
     }
     &__submit-button {
@@ -140,6 +135,11 @@ export default defineComponent({
     background: #ffffff;
     box-shadow: 0px 0px 10px #ebf0ff;
     border-radius: 24px;
+  }
+  &__input-wrapper {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
   }
   &__form-link {
     text-decoration: none;
