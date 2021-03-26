79713f618d80a81ce092a138e2eb5a78d1c7afe4
murad.ars <murad-arslangiraev-1994@mail.ru>
2021-03-26 12:00:55 +0300

    Правки по названиям класса, перенёс функции в setup.

--- src/layouts/MainLayout/components/Header.vue
+++ src/layouts/MainLayout/components/Header.vue
@@ -1,24 +1,38 @@
 <template>
   <header class="header-main-layout">
     <div class="header-main-layout__left">
-      <img src="@/assets/img/logo.svg" alt="logo">
+      <img
+        src="@/assets/img/logo.svg"
+        alt="logo">
     </div>
     <div class="header-main-layout__right">
-      <p class="header-main-layout__right-showcase">
+      <p class="header-main-layout__showcase">
         Витрина
       </p>
-      <img class="header-main-layout__right-notification" src="@/assets/img/bell.svg" alt="bell">
-      <div class="header-main-layout__user" @click="showHeadPanel">
-        <img src="@/assets/img/user.svg" alt="user">
-        <img class="icon-arrow" src="@/assets/img/down-arrow.svg" alt="arrow-down">
-        <div class="header-main-layout__sub-list" v-if="showModal">
+      <img
+        class="header-main-layout__notification"
+        src="@/assets/img/bell.svg"
+        alt="bell">
+      <div
+        class="header-main-layout__user"
+        @click="showHeadPanel">
+        <img
+          src="@/assets/img/user.svg"
+          alt="user">
+        <img
+          class="header__icon-arrow"
+          src="@/assets/img/down-arrow.svg"
+          alt="arrow-down">
+        <div
+          v-if="showModal"
+          class="header-main-layout__sub-list">
           <router-link
-            class="menu-main-layout__sub-link menu-sub-layout__link"
+            class="menu-main-layout__sub-link"
             :to="{ name: RouteNames.PROFILE }">
             Профиль
           </router-link>
           <button
-            class="menu-main-layout__sub-link"
+            class="header-main-layout__button"
             @click="logOut">
             Выйти
           </button>
@@ -32,36 +46,26 @@
 /**
  * Шапка на основном экране
  */
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { ActionType } from "@/store/modules/User/ActionType";
 import { useRouter } from "vue-router";
 import { useStore } from "@/store";
 import { RouteNames } from "@/router/RouteNames";
 
-// const menuLinks = [{ routeName: RouteNames.PROFILE, name: "Профиль" }, { routeName: RouteNames.SIGN_UP, name: "Выйти" }];
 export default defineComponent({
   name: "HeaderMainLayout",
-  data() {
-    return {
-      showModal: false,
-    };
-  },
   setup() {
+    const showModal = ref(false);
     const router = useRouter();
     const store = useStore();
     const logOut = () => {
       store.dispatch(ActionType.SIGN_OUT);
       router.push({ name: RouteNames.SIGN_IN });
     };
-    return { RouteNames, logOut };
-  },
-  methods: {
-    showHeadPanel() {
-      this.showModal = !this.showModal;
-    },
-    showModalFunc() {
-      this.showModal = !this.showModal;
-    },
+    function showHeadPanel() {
+      showModal.value = !showModal.value;
+    }
+    return { RouteNames, logOut, showHeadPanel, showModal };
   },
 });
 </script>
@@ -79,14 +83,14 @@ export default defineComponent({
   &__right {
     display: flex;
     align-items: center;
-    &-showcase {
-      margin-right: 10px;
-      color: white;
-    }
-    &-notification {
-      margin-right: 10px;
-      cursor: pointer;
-    }
+  }
+  &__showcase {
+    margin-right: 10px;
+    color: white;
+  }
+  &__notification {
+    margin-right: 10px;
+    cursor: pointer;
   }
   &__user {
     display: flex;
@@ -95,7 +99,7 @@ export default defineComponent({
     &:hover {
       cursor: pointer;
     }
-    .icon-arrow {
+    .header__icon-arrow {
       margin-left: 5px;
     }
   }
@@ -105,6 +109,21 @@ export default defineComponent({
     left: -80px;
     top: 45px;
     background-color: #1F2328;
+  }
+  &__button {
+    display: block;
+    text-align: left;
+    width: 100%;
+    padding: 10px 30px;
+    color: var(--color-text);
+    text-decoration: none;
+    line-height: 120%;
+    font-size: var(--font-size-navigation-link);
+    background: transparent;
+    transition: 0.3s;
+    &:hover {
+      background: var(--select-navigation-color);
+    }
   }
 }
 </style>
