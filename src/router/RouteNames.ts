a44931eb773d6093a4d98b849f3f70b5ab45cd3f
globalshop <websegment.mail@gmail.com>
2021-03-29 18:42:51 +0500

    hotfix

--- src/router/RouteNames.ts
+++ src/router/RouteNames.ts
@@ -2,6 +2,7 @@ export enum RouteNames {
   MAIN_LAYOUT = "MAIN_LAYOUT",
   MAIN_PAGE = "MAIN_PAGE",
   PRODUCTS = "PRODUCTS",
+  PRODUCT = "PRODUCT",
   PROFILE = "PROFILE",
   SIGN_IN = "SIGN_IN",
   SIGN_UP = "SIGN_UP",
