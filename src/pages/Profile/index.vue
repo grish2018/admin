dc8bc0b13395457ec422001acf4674b7f7055781
Sergey <shadow_mag@mail.ru>
2021-03-30 09:08:21 +0000

    feat(context): profile WIP

--- src/pages/Profile/index.vue
+++ src/pages/Profile/index.vue
@@ -1,32 +1,41 @@
 <template>
-  <div>Профиль</div>
-  <base-input
-    v-model="user.nickname"
-    :required="false"
-    placeholder="Введите вашу почту"
-    type="text">
-    <template #label>
-      <span>Nickname</span>
-    </template>
-  </base-input>
-  <base-input
-    v-model="user.email"
-    :required="false"
-    placeholder="Введите вашу почту"
-    type="text">
-    <template #label>
-      <span>email</span>
-    </template>
-  </base-input>
-  <base-input
-    v-model="password"
-    :required="false"
-    placeholder="введите пароль"
-    type="text">
-    <template #label>
-      <span>Password</span>
-    </template>
-  </base-input>
+  <div class="page-wrap">
+    <h1 class="page-title">
+      Профиль
+    </h1>
+    <div class="form">
+      <base-input
+        v-model="user.nickname"
+        :required="false"
+        placeholder="Введите вашу почту"
+        type="text">
+        <template #label>
+          <span>Nickname</span>
+        </template>
+      </base-input>
+      <base-input
+        v-model="user.email"
+        :required="false"
+        placeholder="Введите вашу почту"
+        type="text">
+        <template #label>
+          <span>email</span>
+        </template>
+      </base-input>
+      <base-input
+        v-model="password"
+        :required="false"
+        placeholder="введите пароль"
+        type="text">
+        <template #label>
+          <span>Password</span>
+        </template>
+      </base-input>
+      <button class="button">
+        Сохранить
+      </button>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, onBeforeMount, computed, ref } from "vue";
@@ -55,3 +64,32 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+  .page-wrap {
+    padding: 50px;
+    width: 100%;
+  }
+  .page-title {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  .form {
+    margin: 0 auto;
+    width: 400px;
+  }
+  .button {
+    background: #8a2be2;
+    color: #fff;
+    width: 150px;
+    height: 40px;
+    border-radius: 5px;
+    outline: none;
+    transition: background .3s;
+    margin: 0 auto;
+    display: block;
+    &:hover {
+      background: #943ee4;
+    }
+  }
+</style>
