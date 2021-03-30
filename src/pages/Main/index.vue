db224d89d40c9a54863217a9d265bcb0ed490df0
alex-rak <2304alex2304@gmail.com>
2021-03-30 23:51:17 +0500

    ADMIN-12: add button for test modal

--- src/pages/Main/index.vue
+++ src/pages/Main/index.vue
@@ -1,19 +1,45 @@
 <template>
   <div class="main-page">
     {{ $t("Home") }}
+    <button
+      class="main-page__button"
+      @click="openModal">
+      OpenModal
+    </button>
+    <modal-window
+      v-if="isOpenModal"
+      @close="closeModal" />
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-
+import { defineComponent, ref } from "vue";
+import ModalWindow from "./Components/Modal.vue";
 export default defineComponent({
   name: "MainPage",
+  components: {
+    ModalWindow,
+  },
+  setup() {
+    const isOpenModal = ref(false);
+    function openModal() {
+      isOpenModal.value = true;
+    }
+    function closeModal() {
+      isOpenModal.value = false;
+    }
+    return { isOpenModal, openModal, closeModal };
+  },
 });
 </script>
 
 <style lang="scss">
 .main-page {
   display: flex;
+  width: 100%;
+  &__button {
+    max-height: 50px;
+    background: #8c8cff;
+  }
 }
 </style>
