6e1cad55db289abcc540881e73f1abfd3e6b8130
murad.ars <murad-arslangiraev-1994@mail.ru>
2021-03-31 16:23:24 +0300

    ADMIN-15. Работа над интернационализацией, на проверку Александру.

---
+++ src/plugins/Translation/languages.ts
@@ -0,0 +1,1 @@
+export const languages: any[] = ["en", "ru"];
--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,13 +1,46 @@
-import { getStorage } from "@/utils/storage";
+import { getStorage, setStorage } from "@/utils/storage";
 import { createI18n } from "vue-i18n";
-import { en } from "./Translation/en";
-import { ru } from "./Translation/ru";
+import { languages } from "@/plugins/Translation/languages";
+// import { en } from "./Translation/en";
+// import { ru } from "./Translation/ru";
+//
+// const locale = getStorage("locale");
+// export const i18n = createI18n({
+//   locale: locale || "ru",
+//   messages: {
+//     en: en,
+//     ru: ru,
+//   },
+// });
 
-const locale = getStorage("locale");
-export const i18n = createI18n({
-  locale: locale || "ru",
-  messages: {
-    en: en,
-    ru: ru,
-  },
-});
+export const i18n = createI18n({});
+const locale = getStorage("locale") || "en";
+const loadedLanguages: any[] = [];
+
+function setI18Language(lang: any) {
+  i18n.global.locale = lang;
+  setStorage("locale", lang);
+  return lang;
+}
+
+export function loadedLanguagesAsync(lang: any) {
+  if (i18n.global.locale === lang) {
+    return Promise.resolve(setI18Language(lang));
+  }
+  if (loadedLanguages.includes(lang)) {
+    return Promise.resolve(setI18Language(lang));
+  }
+  if (!languages.includes(lang)) {
+    lang = "en";
+  }
+
+  return import(`@/plugins/Translation/${lang}`).then(
+    (messages) => {
+      i18n.global.setLocaleMessage(lang, messages);
+      loadedLanguages.push(lang);
+      return setI18Language(lang);
+    }
+  );
+}
+
+loadedLanguagesAsync(locale);
